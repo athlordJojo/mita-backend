2c5e6c3051117c426121168c7e25871e6b150d1a
Raychouni Joan | medneo <joan.raychouni@medneo.com>
2018-10-16 15:32:13 +0200

    add api for group

--- src/main/kotlin/com/jachouni/mitabackend/controllers/KindergardenGroupController.kt
+++ src/main/kotlin/com/jachouni/mitabackend/controllers/KindergardenGroupController.kt
@@ -7,6 +7,7 @@ import com.jachouni.mitabackend.entities.repositories.CustomerRepository
 import com.jachouni.mitabackend.entities.repositories.KindergardenGroupRepository
 import com.jachouni.mitabackend.entities.repositories.KindergardenRepository
 import com.jachouni.mitabackend.services.KindergardenGroupService
+import org.modelmapper.ModelMapper
 import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.http.HttpStatus
 import org.springframework.http.ResponseEntity
@@ -18,24 +19,21 @@ import javax.persistence.EntityNotFoundException
 @RestController
 class KindergardenGroupController(@Autowired val kindergardenGroupService: KindergardenGroupService) {
 
-//    @PostMapping(value = ["/customers/{customer-id}/kindergardens/{kindergarden-id}/kindergardengroup"])
-//    @ResponseBody
-//    fun createKindergardenGroup(@PathVariable("customer-id") customerId: UUID, @PathVariable("kindergarden-id") kindergardenId: UUID, @RequestBody dto: KindergardenGroupDto) {
-//        if (kindergarden.isPresent) {
-//            val k = kindergarden.get()
-//            k.addKindergardenGroup(KindergardenGroup(name = dto.name, kindergarden = k, groupbook = Groupbook("Groubbook of group ${k.name}")))
-//        } else {
-//            throw EntityNotFoundException("Could not find kindergarden with id: $kindergardenId")
-//        }
-//    }
+    @PostMapping(value = ["/customers/{customer-id}/kindergardens/{kindergarden-id}/kindergardengroup"])
+    @ResponseBody
+    fun createKindergardenGroup(@PathVariable("customer-id") customerId: UUID, @PathVariable("kindergarden-id") kindergardenId: UUID, @RequestBody dto: KindergardenGroupDto): ResponseEntity<KindergardenGroupDto> {
+        val modelMapper = ModelMapper()
+        val kindergardenGroup = modelMapper.map(dto, KindergardenGroup::class.java)
+        val updatedKindergardenGroup = kindergardenGroupService.createKindergardenGroup(customerId, kindergardenId, kindergardenGroup)
+
+        return ResponseEntity(modelMapper.map(updatedKindergardenGroup, KindergardenGroupDto::class.java), HttpStatus.OK)
+    }
 
     @GetMapping(value = ["/customers/{customer-id}/kindergardens/{kindergarden-id}/kindergardengroup"])
     @ResponseBody
     fun getKindergardenGroups(@PathVariable("customer-id") customerId: UUID, @PathVariable("kindergarden-id") kindergardenId: UUID): ResponseEntity<List<KindergardenGroupDto>> {
         val kindergardens = kindergardenGroupService.getAllKindergarden(customerId, kindergardenId)
         val dtos = kindergardens.map { KindergardenGroupDto(id = it.id, name = it.name) }.toList()
-        return ResponseEntity(dtos, HttpStatus.CREATED)
+        return ResponseEntity(dtos, HttpStatus.OK)
     }
-
-
 }
